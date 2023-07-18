fafa7ae7132df678f6dba301767da756b185104c
stefanotuv <ste.tuveri@gmail.com>
2023-07-18 15:10:48 +0100

    until now the code works. we are modifying the login/logout functionalities for the next commit

--- app/src/main/java/com/example/mainrobot/LoginActivity.kt
+++ app/src/main/java/com/example/mainrobot/LoginActivity.kt
@@ -1,6 +1,5 @@
-package com.example.mainrobot
-//class before the code to manage the user login across the app
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,6 +7,8 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mainrobot.MainActivity
+import com.example.mainrobot.R
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var usernameEditText: EditText
@@ -15,10 +16,19 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var confirmPasswordEditText: EditText
     private lateinit var loginButton: Button
     private lateinit var switchTextView: TextView
-    private var isRegisterMode: Boolean = false
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences("login_prefs", MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false)
+
+        if (isLoggedIn) {
+            startMainActivity()
+            return
+        }
+
         setContentView(R.layout.activity_login)
 
         usernameEditText = findViewById(R.id.usernameEditText)
@@ -31,7 +41,7 @@ class LoginActivity : AppCompatActivity() {
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
 
-            if (isRegisterMode) {
+            if (isRegisterMode()) {
                 val confirmPassword = confirmPasswordEditText.text.toString()
                 if (password != confirmPassword) {
                     Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
@@ -44,16 +54,8 @@ class LoginActivity : AppCompatActivity() {
                 val isValidLogin = validateLogin(username, password)
 
                 if (isValidLogin) {
-                    // Set the login status
-                    val sharedPreferences = getSharedPreferences("login_prefs", MODE_PRIVATE)
-                    val editor = sharedPreferences.edit()
-                    editor.putBoolean("is_logged_in", true)
-                    editor.apply()
-
-                    // Start the MainActivity
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    sharedPreferences.edit().putBoolean("is_logged_in", true).apply()
+                    startMainActivity()
                 } else {
                     Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
                 }
@@ -61,35 +63,44 @@ class LoginActivity : AppCompatActivity() {
         }
 
         switchTextView.setOnClickListener {
-            isRegisterMode = !isRegisterMode
-            if (isRegisterMode) {
-                confirmPasswordEditText.visibility = View.VISIBLE
-                loginButton.text = "Register"
-                switchTextView.text = "Already have an account? Login here."
-            } else {
-                confirmPasswordEditText.visibility = View.GONE
-                loginButton.text = "Login"
-                switchTextView.text = "Don't have an account? Register here."
-            }
+            setRegisterMode(!isRegisterMode())
         }
+
+        setRegisterMode(isRegisterMode())
     }
 
     private fun validateLogin(username: String, password: String): Boolean {
-        // Implement your login validation logic here
-        // Return true if the login is successful, false otherwise
-        // For example:
         val validUsername = "admin"
         val validPassword = "123456"
-
         return username == validUsername && password == validPassword
     }
 
     private fun registerUser(username: String, password: String) {
-        // Implement your registration logic here
-        // Add code to create a new user account with the provided username and password
         Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
-        // You can also automatically log in the user after registration if needed
-        // and proceed to the main screen
+    }
+
+    private fun isRegisterMode(): Boolean {
+        return sharedPreferences.getBoolean("is_register_mode", false)
+    }
+
+    private fun setRegisterMode(registerMode: Boolean) {
+        sharedPreferences.edit().putBoolean("is_register_mode", registerMode).apply()
+        if (registerMode) {
+            confirmPasswordEditText.visibility = View.VISIBLE
+            loginButton.text = "Register"
+            switchTextView.text = "Already have an account? Login here."
+        } else {
+            confirmPasswordEditText.visibility = View.GONE
+            loginButton.text = "Login"
+            switchTextView.text = "Don't have an account? Register here."
+        }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
+
 
